493fb2a36e7832e1fcd37d644e48134361656e54
krarpit <kumar.arpit77@yahoo.com>
2024-08-12 12:00:11 -0700

    error handling -- I miss rust :(

--- analytics/__main__.py
+++ analytics/__main__.py
@@ -9,9 +9,12 @@ logger = logging.getLogger(__name__)
 def main():
     analytics.initialize_config_and_logging()
     start_metrics_server()
-    client = Client()
-    recv = client.send_message(MAESTRO_RESOURCE, MAESTRO_OPEN_FIST)
-    logger.info(f"Received response from GPM; Response={recv}")
+    try:
+        client = Client()
+        recv = client.send_message(MAESTRO_RESOURCE, MAESTRO_OPEN_FIST)
+        logger.info(f"Received response from GPM; Response={recv}")
+    except ConnectionRefusedError:
+        logger.error("Connection to GPM refused -- is the GPM server up and running?")
 
 if __name__ == "__main__":
     main()
