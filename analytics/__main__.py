5195ee548a6bc0855f81e15af8b3bf33a3784d6b
krarpit <kumar.arpit77@yahoo.com>
2024-08-12 21:32:41 -0700

    final touches

--- analytics/__main__.py
+++ analytics/__main__.py
@@ -10,9 +10,10 @@ logger = logging.getLogger(__name__)
 
 def main():
     analytics.initialize_config_and_logging()
-    start_metrics_server()
+    metrics_thread = Thread(target=start_metrics_server)
+    metrics_thread.start()
     adc_reader = MockAdcReader()
-    reader_thread = Thread(target = adc_reader.start_reading)
+    reader_thread = Thread(target=adc_reader.start_reading)
     reader_thread.start()
     emg_processor = EmgProcessor(adc_reader)
     emg_processor.run_detect_activation_loop()
