b251d22ad5e2905e54dd7f2c77a125b5b30ce714
Tiaan <89718829+tiaan-b@users.noreply.github.com>
2024-08-15 22:55:32 -0700

    renamed index to start and size to max_size, and made both private. added max_size property

--- analytics/common/circularlist.py
+++ analytics/common/circularlist.py
@@ -10,35 +10,39 @@ class CircularList(object):
     buffer is full (i.e discarding oldest data without having to reallocate or traverse the
     entire buffer).
     """
-    def __init__(self, size, data = []):
-        if size <= 0:
-            raise InvalidSize("A list cannot have a negative or zero size.")
-        self.index = 0
-        self.size = size
-        self._data = list(data)[-size:]
+    def __init__(self, max_size, data = []):
+        if max_size <= 0:
+            raise InvalidSize("A list cannot have a negative or zero maximum size.")
+        self._start = 0
+        self._max_size = max_size
+        self._data = list(data)[-max_size:]
 
-    def append(self, value):
-        if len(self._data) == self.size:
-            self._data[self.index] = value
+    def append(self, value) -> None:
+        if len(self._data) == self._max_size:
+            self._data[self._start] = value
         else:
             self._data.append(value)
-        self.index = (self.index + 1) % self.size
+        self._start = (self._start + 1) % self._max_size
 
     def __getitem__(self, key):
-        if len(self._data) == self.size:
-            return(self._data[(key + self.index) % self.size])
+        if len(self._data) == self._max_size:
+            return(self._data[(key + self._start) % self._max_size])
         else:
             return(self._data[key])
 
     def __repr__(self):        
-        return (self._data[self.index:] + self._data[:self.index]).__repr__() + ' (' + str(len(self._data))+'/{} items)'.format(self.size)
+        return (self._data[self._start:] + self._data[:self._start]).__repr__() + ' (' + str(len(self._data))+'/{} items)'.format(self._max_size)
     
     def __len__(self):
-        return self.size
+        return self._max_size
     
     def __array__(self) -> np.ndarray:
         """
         This method is needed to satisfy the `array-like` bound required by the 
         `np.array` method.
         """
-        return np.array(self._data[self.index:] + self._data[:self.index])
+        return np.array(self._data[self._start:] + self._data[:self._start])
+    
+    @property
+    def max_size(self) -> int:
+        return self._max_size
