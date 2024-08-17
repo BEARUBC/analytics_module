e3aa7f7999eeb3dab2e24d3a55dda337880c3c05
Tiaan <89718829+tiaan-b@users.noreply.github.com>
2024-08-17 13:50:15 -0700

    added mock adc option to config

--- analytics/__main__.py
+++ analytics/__main__.py
@@ -1,17 +1,23 @@
 import analytics
 import logging
+from analytics import config
 from analytics.gpm.constants import *
 from analytics.metrics.exporter import start_metrics_server
-from analytics.adc.mockreader import MockAdcReader
 from analytics.processing.filters import EmgProcessor
 from analytics.adc.visualization import EmgVisualizer
 from threading import Thread
 
 logger = logging.getLogger(__name__)
 
+if config["adc"]["use_mock_adc"]:
+    logger.info("Using mock ADC")
+    from analytics.adc.mockreader import MockAdcReader as Reader
+else:
+    from analytics.adc.reader import AdcReader as Reader
+
 def main():
     analytics.initialize_config_and_logging()
-    adc_reader = MockAdcReader()
+    adc_reader = Reader()
     emg_processor = EmgProcessor(adc_reader)
     Thread(target=adc_reader.start_reading).start()
     Thread(target=emg_processor.run_detect_activation_loop).start()
--- analytics/configs/adcconfig.py
+++ analytics/configs/adcconfig.py
@@ -6,5 +6,6 @@ ADC_CONFIG_TEMPLATE = {
     "inner_read_buffer_size": confuse.Optional(int, 2000),
     "outer_read_buffer_size": confuse.Optional(int, 2000),
     "mock_reader_state_buffer_size": confuse.Optional(int, 100),
-    "sleep_between_reads_in_seconds": confuse.Optional(int, 0.1)
+    "sleep_between_reads_in_seconds": confuse.Optional(int, 0.1),
+    "use_mock_adc": confuse.Optional(bool, False)
 }
