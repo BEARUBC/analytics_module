8eed8fc6432abdb3f84257cad75326e0577d08f5
rMacD64 <91086955+rMacD64@users.noreply.github.com>
2024-10-05 11:37:12 -0700

    Update filters.py

--- analytics/processing/filters.py
+++ analytics/processing/filters.py
@@ -190,5 +190,5 @@ class EmgProcessor:
         inner, outer = self._buffers
         return (inner, outer)
 
-    def get_threholds(self):
+    def get_thresholds(self):
         return self.inner_threshold, self.outer_threshold
